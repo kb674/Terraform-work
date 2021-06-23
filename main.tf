733251c16ff3a7d540a22d340c6327aa676fe199
Ubuntu <kusha@cli-machine.e0w5jh55ta1utijfaq0v0zt43g.zx.internal.cloudapp.net>
2021-06-23 14:12:17 +0000

    Updated. Includes nsg and nsg rules.

--- main.tf
+++ main.tf
@@ -32,9 +32,49 @@ resource "azurerm_subnet" "frontend" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_network_security_group" "frontend-nsg" {
+  name                = "frontend-nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+}
+
+resource "azurerm_network_security_rule" "frontend-nsg-rule" {
+  name                        = "ssh-from-any-location"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.frontend-nsg.name
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+}
+
 resource "azurerm_subnet" "backend" {
   name                 = "backend"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "backend-nsg" {
+  name                = "backend-nsg"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+}
+
+resource "azurerm_network_security_rule" "backend-nsg-rule" {
+  name                        = "ssh-from-public-subnet"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.backend-nsg.name
+  priority                    = 102
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "10.0.1.0/24"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
 }
